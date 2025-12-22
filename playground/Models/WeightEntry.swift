425b9533fd003d9edf1b506d86518b5cdad4122a
bassamHillo <48155139+bassamHillo@users.noreply.github.com>
2025-12-22 18:06:08 +0200

    Fix

--- playground/Models/WeightEntry.swift
+++ playground/Models/WeightEntry.swift
@@ -9,20 +9,6 @@ import Foundation
 import SwiftData
 
 @Model
-final class WeightEntry: Identifiable {
-    var id: UUID
-    var weight: Double // in lbs
-    var date: Date
-    
-    init(id: UUID = UUID(), weight: Double, date: Date = Date()) {
-        self.id = id
-        self.weight = weight
-        self.date = date
-    }
-}
-
-/// Represents a single weight entry for tracking progress
-@Model
 final class WeightEntry {
     var id: UUID
     var weight: Double // in kg
