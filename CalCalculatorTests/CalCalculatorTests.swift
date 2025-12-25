77b22ad91c656ecd2491f536334c5615898fe86f
zoharb157 <59223810+zoharb157@users.noreply.github.com>
2025-12-25 16:02:16 +0200

    Configure test target and add Live Activities support
    - Added NSSupportsLiveActivities key to Info.plist
    - Configured CalCalculatorTests target in scheme
    - Enabled parallel test execution
    - All 206 tests are now configured and ready to run

--- CalCalculatorTests/CalCalculatorTests.swift
+++ CalCalculatorTests/CalCalculatorTests.swift
@@ -2,27 +2,15 @@
 //  CalCalculatorTests.swift
 //  CalCalculatorTests
 //
-//  Unit tests for CalCalculator app
+//  Created by Zohar Buchris on 2025-12-25.
 //
 
-import XCTest
-@testable import playground
-import SwiftData
+import Testing
 
-final class CalCalculatorTests: XCTestCase {
-    
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+struct CalCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-    
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertTrue(true)
-    }
+
 }
-
